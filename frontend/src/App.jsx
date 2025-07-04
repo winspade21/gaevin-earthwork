be00aa8838ba5a55749d6d4193a3d31b209e164a
cabauatanjulius <cabauatanjulius@gmail.com>
2025-07-05 00:40:51 +0800

    added blog and project routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
              <Route path='/' element={<Home/>}/>
              <Route path='/about' element={<About/>}/>
              <Route path='/services' element={<Services/>}/>
-             <></>
+             
              <Route path='/projects' element={<Projects/>}/>
              <Route path='/blogs' element={<Blogs/>}/>
           </Routes> 
